b7b93cb134d363d57095d6966e3adf86040a99d8
Kla1ser <olegdeep10@gmail.com>
2025-10-18 23:11:56 +0200

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace MomentSnap
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
